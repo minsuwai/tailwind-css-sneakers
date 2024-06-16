76edbcbd70b0ca12926e1b15d16f1f2f03375011
Minsuwai <mswwai15@gmail.com>
2024-06-16 15:01:38 +0630

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,19 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    //   extend: {
+    //     colors: {
+    //       purple: {
+    //         951: "#5755eb",
+    //       },
+    //     },
+    //     backgroundImage: {
+    //       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+    //       "gradient-conic":
+    //         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    //     },
+    //   },
+    // },
   },
   plugins: [],
 };
